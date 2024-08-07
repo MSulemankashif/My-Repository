cfb8070f7b25ec291d2aa2e38bf8af8eb6966789
M Suleman <163521322+MShahSuleman@users.noreply.github.com>
2024-08-07 18:05:19 +0500

    Update js-practice.js

--- js-practice.js
+++ js-practice.js
@@ -1,6 +1,31 @@
 
-for(i = 1; 1 <= 10; i++)
-{
-    console.log(i , "Bharwa Usman Irshad " , "<br>");
+// for(i = 1; i <= 100; i++)
+// {
+//     console.log(i , " :Suleman" , "<br>");
     
-}
+// }
+let namee = "Suleman";
+let age = 20
+
+// document.write("Hello <b>" + namee  + "</b> thank you for contacting us (your age is) " + age + " we will be back as soon as possible ...")
+
+
+let a = 10;
+let b = 9;
+let c = 10;
+
+let cond1 = a === b;
+let cond2 = a > b;
+
+// console.log(cond1 && cond2);    // Logical And Operator
+// console.log(cond1 || cond2);    //Logical OR
+//  console.log(cond1 != cond2);    //Logical NOT
+
+
+//Loops
+
+let stds = ["Ali", "Usama", "Asad"];
+
+stds.forEach(function(std) {
+    document.write(std + "<br>");
+});
