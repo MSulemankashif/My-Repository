d3b4cccef6051e5f4c92ab9fdb0e672d1483032f
M Suleman <163521322+MShahSuleman@users.noreply.github.com>
2024-08-08 17:40:08 +0500

    Update js-practice.js

--- js-practice.js
+++ js-practice.js
@@ -1,8 +1,8 @@
-for(i = 1; i <= 100; i++)
-{
-    document.write(i , " : Suleman" , "<br>");
+// for(i = 1; i <= 100; i++)
+// {
+//     document.write(i , " : Suleman" , "<br>");
     
-}
+// }
 let namee = "Suleman"
 let age = 18
 
@@ -30,5 +30,49 @@ let cond2 = a > b;
 // });
 
 //Ternary Operators
-let result = age >= 18 ? "adult" : "not adult";
-console.log(result);
+// let result = age >= 18 ? "adult" : "not adult";
+// console.log(result);
+
+
+// let namee1 = prompt("Enter your Name Here");
+// document.write("Hello <b>" + namee1 + "</b> Good to have you here we will contact you as soon as possible...");
+
+// let num = prompt("Enter a number")
+
+// if(num % 5 === 0)
+// {
+//     console.log(num, "is a mutiple of 5");
+    
+// } else{
+//     console.log(num, "is not a mutiple of 5");
+// }
+
+let score = prompt("Enter Your Marks" )
+let grade;
+
+if (score >= 90 && score <=100 )
+{
+    grade="A";
+}else if (score >= 70 && score <= 89)
+{
+    grade = "B";
+}
+else if (score >= 50 && score <= 69)
+    {
+        grade = "C";
+    }
+else if (score >= 30 && score <= 49)
+{
+    grade = "D";
+}
+else
+{
+    console.log("Please Enter Corect Marks");
+    
+}
+
+document.write("your Grade is ", grade ," According to your percenteage" );
+
+
+
+
