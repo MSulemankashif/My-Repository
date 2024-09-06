5630aaaf1e7ec5e7d05b4765915a76b09c0abb43
M Suleman <163521322+MShahSuleman@users.noreply.github.com>
2024-09-06 19:04:25 +0500

    DOM

--- DOM/script.js
+++ DOM/script.js
@@ -1,1 +1,18 @@
-document.querySelector("div").children;
+
+// let arr = ["Karachi","Lahore","Islamabad","Peshawar"];
+
+// arr.forEach((val,index,arr) =>{
+//     console.log(val.toUpperCase(),index,arr);
+// }
+// )
+
+// arr.forEach((val,index)=>
+// { 
+//     console.log(index,val);
+// })
+
+// let h2 = document.querySelector("h2");
+// console.log(h2);
+
+let div = document.querySelectorall("box");
+console.dir(div);
