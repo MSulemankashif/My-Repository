01e113e24011a9157798c68e238dde1b5c9e69a7
M Suleman <163521322+MShahSuleman@users.noreply.github.com>
2024-10-03 17:16:32 +0500

    Events in JS

--- DOM/script.js
+++ DOM/script.js
@@ -27,19 +27,47 @@
 // let h2 = document.querySelector("h2");
 // console.log(h2);
 
-let button = document.querySelector(".btn1");
-let button1 = document.querySelector(".btn2");
+// let button = document.querySelector(".btn1");
+// let button1 = document.querySelector(".btn2");
+// let body = document.querySelector("body");
+// let nav = document.querySelector("#none");
+
+// button.addEventListener("click", ()=>{
+//     body.style.backgroundColor = "white";
+//     body.style.color = "black";
+//     nav.classList("btn", "btn-outline-success");
+//   });
+// button1.addEventListener("click",()=>
+//     {
+//         body.style.backgroundColor="black";
+//         body.style.color="white";
+//      });
+        
+// let btn = document.querySelector(".btn-outline-success");
+
+// let handler = ()=> {
+//     console.log("Button is clicked!");
+    
+// }
+
+// btn.addEventListener("click", handler);
+
+let modeBtn = document.querySelector(".btn");
 let body = document.querySelector("body");
+let mode = "light";
 
-button.addEventListener("click",()=>
-{
-    body.style.backgroundColor="white";
-    body.style.color="black";
-})
+modeBtn.addEventListener("click",()=>{
+    if(mode === "light")
+    {
+        mode = "dark";
+        body.classList.add("dark");
+        modeBtn.style.color="white";
+        body.classList.remove("light");
+    } else  {  
+        mode = "light";
+        body.classList.add("light");
+        modeBtn.style.color="black";
+        body.classList.remove("dark");
+    }     
 
-button1.addEventListener("click",()=>
-{
-    body.style.backgroundColor="black";
-    body.style.color="white";
-})
-
+});
