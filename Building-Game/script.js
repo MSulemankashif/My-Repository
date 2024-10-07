569a87d4283b9a6d9b614577d40c95e31eb614d0
M Suleman <163521322+MShahSuleman@users.noreply.github.com>
2024-10-07 17:10:43 +0500

    Building Game

---
+++ Building-Game/script.js
@@ -0,0 +1,13 @@
+// let namee = "Suleman";
+// let lastName = "kashif"
+// let fullName = (namee + lastName);
+// console.log(`Your username is @${fullName}${fullName.length + 3}`);
+
+
+let arr=["karachi","lahore","islamabad","Peshawar","Multan"];
+
+arr.forEach((val,index)=>
+{
+    console.log(index,val);
+    
+})
