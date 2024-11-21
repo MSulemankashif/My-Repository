46460f90adb7314b35ced3c7ec582f43f8241a0b
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2024-11-21 11:41:12 +0500

    update

---
+++ Learning-JQuery/src/jquery.js
@@ -0,0 +1,46 @@
+" Hide Unhide Element"
+// $(document).ready(function(){
+//     $("p").hover(function(){
+//         $(this).css("cursor","pointer");
+//     });
+//     $("p").click(function(){
+//         $(this).hide();
+//     });
+//     $("#toggleButton").click(function(){
+//         $("h1").hide(400);
+//     });
+//     $("#showBtn").click(function(){
+//         $("h1").show(400);
+//     });
+// });
+"Basic"
+// console.log(`We are Using ${jQuery}`);
+// $("p").click(function(){
+//     $("p").hide();
+// })
+// $("p").click(function(){
+//     alert(`Button is Clicked!`)
+// })
+
+    "Show hide Element"
+// $(document).ready(function(){
+//     $("#toggleButton").click(function(){
+//         $("p").hide(700);
+//     });
+//     $("#showBtn").click(function(){
+//         $("p").show(500);
+//     });
+// });
+
+"Applying CSS"
+// $("p").hover(
+//     function() { $(this).css("background-color", "yellow"); },
+//     function() { $(this).css("background-color", "white"); }
+// );
+
+// $("#toggleButton").hover(
+//     function(){$(this).css("background-color","purple")},
+//     function(){$(this).css("background-color","white")}
+// );
+
+
