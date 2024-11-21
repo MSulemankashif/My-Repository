402a9723b4c07f8dbf07354073eed98dce21317a
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2024-11-21 14:05:45 +0500

    update

--- Learning-JQuery/src/jquery.js
+++ Learning-JQuery/src/jquery.js
@@ -48,14 +48,14 @@ let body = document.querySelector("body");
 let mode = "light";
 
 button.addEventListener("click",()=>{
-    if ( mode === "light"){
+    if(mode === "light"){
         mode = "dark";
         body.classList.add("dark");
         body.classList.remove("light");
-    }else{
-        mode = "light";
+    } else{
+        mode = "light"
         body.classList.add("light");
-        body.classList.remove("dark");
+        body.classList.remove("dark")
     } console.log(mode);
     
 })
