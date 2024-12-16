139eb472009b7574b4243cf3d2c2f245cecc8156
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2024-12-16 15:30:05 +0500

    Fetch API

--- API/app.js
+++ API/app.js
@@ -5,9 +5,35 @@ const URL = "https://cat-fact.herokuapp.com/facts";
 // let response = await fetch(URL);
 //     console.log(response);
 // };
+
     // Using IFFE
-(async ()=>{
-    console.log(`Getting data.......`);
-let response = await fetch(URL);
-    console.log(response);
-})();
+// (async ()=>{
+//     console.log(`Getting data.......`);
+// let response = await fetch(URL);
+//     console.log(response);
+//     let data = await response.json();
+//     // console.log(data[2].text);
+//     document.getElementById("api").innerText=(data[2].text)
+// })();
+
+// let getFacts = async ()=>{
+//     console.log(`Getting Data.....`);
+//     let response = await fetch(URL);
+//     console.log(response);
+//     let data = await response.json();
+//     console.log(data);
+//     document.getElementById("api").innerText=data[0].text
+// };
+//Promise Chaining
+function getFacts(){
+    fetch(URL).then((Response)=>{
+        return Response.json();
+    })
+    .then((data)=>{
+        console.log(data);
+        document.querySelector("#api").innerText=(data[0].text)
+    });
+};
+
+let btn = document.querySelector(".btn");
+btn.addEventListener('click',getFacts)
