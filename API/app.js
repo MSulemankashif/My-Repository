faacb8d848bd1116602690dfcd320a424f2554a4
Shah Suleman <163521322+MSulemankashif@users.noreply.github.com>
2024-12-28 13:27:05 +0500

    API

--- API/app.js
+++ API/app.js
@@ -1,10 +1,10 @@
 const URL = "https://cat-fact.herokuapp.com/facts";
 
-// let getFacts = async ()=>{
-//     console.log(`Getting data.......`);
-// let response = await fetch(URL);
-//     console.log(response);
-// };
+let getFacts = async ()=>{
+    console.log(`Getting data.......`);
+let response = await fetch(URL);
+    console.log(response);
+};
 
     // Using IFFE
 // (async ()=>{
@@ -25,24 +25,15 @@ const URL = "https://cat-fact.herokuapp.com/facts";
 //     document.getElementById("api").innerText=data[0].text
 // };
 //Promise Chaining
-function getFacts(){
-    fetch(URL).then((Response)=>{
-        return Response.json();
-    })
-    .then((data)=>{
-        console.log(data);
-        document.querySelector("#api").innerText=(data[0].text)
-    });
-};
+// function getFacts(){
+//     fetch(URL).then((Response)=>{
+//         return Response.json();
+//     })
+//     .then((data)=>{
+//         console.log(data);
+//         document.getElementById("api").innerText=(data[0].text)
+//     });
+// };
 
-let btn = document.querySelector(".btn");
-btn.addEventListener('click',getFacts);
-
-
-const base_url = "https://latest.currency-api.pages.dev/v1/currencies/usd.json";
-
-for (code in countryList){
-    console.log(code);
-    
-}
-
+// let btn = document.querySelector(".btn");
+// btn.addEventListener('click',getFacts);
