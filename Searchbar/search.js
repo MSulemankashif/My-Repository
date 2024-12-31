13cd8f953f1aa08b7033769633e22086ed89d098
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2024-12-31 11:55:28 +0500

    Search bar

---
+++ Searchbar/search.js
@@ -0,0 +1,19 @@
+const searchInput = document.getElementById('searchInput');
+const itemList = document.getElementById('itemList');
+const items = itemList.getElementsByTagName('li');
+
+searchInput.addEventListener('input', function(){
+    const filter = searchInput.value.toLowerCase();
+
+    for (let i = 0; i < items.length; i++){
+        const item = items[i];
+        const text = item.textContent || item.innerText;
+        
+        if (text.toLowerCase().indexOf(filter) > -1) {
+            item.style.display= '';
+        }else{
+            item.style.display = 'none';
+        }
+    }
+
+})
