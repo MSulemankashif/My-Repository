18e16ac861079a644befa924c6971e7de0e57540
Shah Suleman <163521322+MSulemankashif@users.noreply.github.com>
2025-02-19 20:26:14 +0500

    Update

--- API-Practice/script-practice.js
+++ API-Practice/script-practice.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 
 let input = document.querySelector("input");
 let img = document.querySelector("img");
@@ -11,3 +12,15 @@ img.addEventListener("click", ()=>{
         img.src = "./eye.svg";
     };
 });
+=======
+let pass = document.querySelector("#pass");
+document.querySelector("img").addEventListener("click",()=>{
+        type =Text;
+        if(type == "pass")
+        {
+            pass.type="pass";
+        } else{
+            pass.type="text";
+        }
+})
+>>>>>>> Stashed changes
