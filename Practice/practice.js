c3d37d50968def22c95d158048f48a480c5f11a0
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2025-08-07 18:43:40 +0500

    Update practice.js

--- Practice/practice.js
+++ Practice/practice.js
@@ -11,12 +11,22 @@
 // const add = (a,b) => a+b;
 // console.log(add(5,10));
 
-let array = [2,4,6,8,10];
+let array = [2,3,4,5,6,7,8,10];
 
-array.forEach((val)=>{
-    console.log(val ** 2);
-    
-})
+"For Square Root"
+// array.forEach((val)=>{
+//     console.log(val ** 2);
+// })
+
+"Map Method (Gives  result in array)"
+// let newArr = array.map((val)=>{
+//     return val **2;
+// })
+
+// console.log(newArr);
+
+"Filter Method"
+
 
 
  
