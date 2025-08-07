c948273a033184757aa41efcc7ca082277543ea8
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2025-08-07 17:10:46 +0500

    Practice Javascript

---
+++ Practice/practice.js
@@ -0,0 +1,13 @@
+let array = ["Karachi", "Lahore", "Islamabad"];
+
+// array.forEach((value1,indexed,array1)=>{
+//     console.log(`The Value is ${value1} at the index of ${indexed}`);
+    
+// });
+
+const add = (a,b) => a+b;
+
+console.log(add(5,10));
+
+ 
+ 
