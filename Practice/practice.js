276a4fa40852a165ef1f8de4d78b1516329fd31f
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2025-08-07 17:17:24 +0500

    Update practice.js

--- Practice/practice.js
+++ Practice/practice.js
@@ -1,13 +1,23 @@
-let array = ["Karachi", "Lahore", "Islamabad"];
+"for each Loop"
+// let array = ["Karachi", "Lahore", "Islamabad"];
 
 // array.forEach((value1,indexed,array1)=>{
 //     console.log(`The Value is ${value1} at the index of ${indexed}`);
     
 // });
 
-const add = (a,b) => a+b;
+"Arrow Function"
 
-console.log(add(5,10));
+// const add = (a,b) => a+b;
+// console.log(add(5,10));
+
+let array = [2,4,6,8,10];
+
+array.forEach((val)=>{
+    console.log(val ** 2);
+    
+})
+
 
  
  
