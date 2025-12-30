bb33963a55bd0874812fb7ac2f31198e23f6517a
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2025-12-30 15:38:42 +0500

    Practice

--- Searchbar/search.js
+++ Searchbar/search.js
@@ -1,18 +1,17 @@
-const searchInput = document.getElementById('searchInput');
-// const itemList = document.getElementById('itemList');
-const items = itemList.getElementsByTagName('li');
+const searchInput = document.getElementById("searchInput");
+const items = itemList.getElementsByTagName("li");
 
-searchInput.addEventListener('input', function(){
+searchInput.addEventListener("input", function (){
     const filter = searchInput.value.toLowerCase();
 
-    for (let i = 0; i < items.length; i++){
+    for (let i = 0; i < items.length; i++) {
         const item = items[i];
-        const text = item.innerText;
-        
-        if (text.toLowerCase().indexOf(filter) > -1) {
-            item.style.display= '';
+        const itemText = item.innerText;
+       
+        if(itemText.toLowerCase().indexOf(filter) > -1){
+            items[i].style.display = '';
         }else{
-            item.style.display = 'none';
-        };
-    };
-});
+            items[i].style.display = 'none';
+        }
+    }
+    });
