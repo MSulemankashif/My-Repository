8602c350f168dcea9a0383f99d42dc0d19b23c46
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2025-12-30 16:27:16 +0500

    update

---
+++ Practice/JsPractice4.js
@@ -0,0 +1,6 @@
+// let arr = ["Karachi","Lahore","Islamabad","Peshawar"];
+
+// arr.forEach((val,index,arr) =>{
+//     console.log(val.toUpperCase(),index,arr);
+// }
+// )
---
+++ Practice2/script.js
@@ -0,0 +1,7 @@
+"For each loop"
+// let arr = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar"];
+
+// arr.forEach((val, index, arr)=> {
+//     console.log(index, val);
+    
+// })
