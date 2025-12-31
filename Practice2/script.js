529f1fc2ab9690f19ae6108b9253c4d99ffa0e68
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2025-12-31 15:38:57 +0500

    Sqaure root practice

--- Practice2/script.js
+++ Practice2/script.js
@@ -1,7 +1,18 @@
-"For each loop"
-// let arr = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar"];
+let input = document.getElementsByTagName('input');
+let h3 = document.getElementsByTagName('h3');
 
-// arr.forEach((val, index, arr)=> {
-//     console.log(index, val);
+document.querySelector('form').addEventListener('submit', function(event){
+    event.preventDefault();
+    let value = input[0].value;
+
+    if(value <= 0) {
+        h3[0].innerText = `Please enter a valid positive number greater than 0`;
+    }else{
+
+        let result = value ** 2;
     
-// })
+        h3[0].innerText = `The square root of ${value} is ${result}`
+    }
+
+    
+})
