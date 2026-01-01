431ca5395f5c2c2ba63c4790b799a4da6f371ce2
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2026-01-01 18:13:42 +0500

    Update script.js

--- Practice2/script.js
+++ Practice2/script.js
@@ -1,16 +1,25 @@
-let input = document.querySelector('input');
-let h3 = document.querySelector('h3');
+'For Getting square root of a number'
+// let input = document.querySelector('input');
+// let h3 = document.querySelector('h3');
 
-document.querySelector('form').addEventListener('submit', function(event){
-    event.preventDefault();
-    let value = input.value;
+// document.querySelector('form').addEventListener('submit', function(event){
+//     event.preventDefault();
+//     let value = input.value;
 
-    if(value <= 0) {
-        h3.innerText = `Please enter a valid positive number greater than 0`;
-    }else{
+//     if(value <= 0) {
+//         h3.innerText = `Please enter a valid positive number greater than 0`;
+//     }else{
 
-        let result = value ** 2;
+//         let result = value ** 2;
     
-        h3.innerText = `The square root of ${value} is ${result}`
-    }
+//         h3.innerText = `The square root of ${value} is ${result}`
+//     }
+// })
+
+"Map Method"
+let array = [2, 4, 6, 8];
+
+array.map((val)=>{
+    val ** 2;
 })
+
