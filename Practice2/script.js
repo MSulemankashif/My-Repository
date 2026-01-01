56f1cf3c0d9c2fa4c1002bb375ff2c7417617200
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2026-01-01 14:56:18 +0500

    h3.[0] returns a first index of a collection

--- Practice2/script.js
+++ Practice2/script.js
@@ -1,16 +1,16 @@
-let input = document.getElementsByTagName('input');
-let h3 = document.getElementsByTagName('h3');
+let input = document.querySelector('input');
+let h3 = document.querySelector('h3');
 
 document.querySelector('form').addEventListener('submit', function(event){
     event.preventDefault();
-    let value = input[0].value;
+    let value = input.value;
 
     if(value <= 0) {
-        h3[0].innerText = `Please enter a valid positive number greater than 0`;
+        h3.innerText = `Please enter a valid positive number greater than 0`;
     }else{
 
         let result = value ** 2;
     
-        h3[0].innerText = `The square root of ${value} is ${result}`
+        h3.innerText = `The square root of ${value} is ${result}`
     }
 })
