71efd3be8e87452bbd68589050c3b7de67cf10fb
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2026-01-04 19:05:51 +0500

    Update

--- Practice2/script.js
+++ Practice2/script.js
@@ -1,6 +1,5 @@
-let count = 0;
-document.querySelector("button").addEventListener("click", function(){
-    
-    count++;
-    this.innerText = `Clicked ${count} times`;  
+$(document).ready(function(){
+    $("p").click(function(){
+        $(this).hide();
+    })
 })
