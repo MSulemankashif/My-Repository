7b16b9e773b010c14bdae0ebbcdf5f3fa9ae6583
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2026-01-04 19:19:38 +0500

    Learning Jquery

--- Practice2/script.js
+++ Practice2/script.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
-    $("p").click(function(){
-        $(this).hide();
+    $("#hide").click(function(){
+        $("p").hide(700);
+    });
+    $("#show").click(function(){
+        $("p").show(400);
+    })
+
+    $("p").hover(function(){
+        $(this).css("background-color", "Lightgray"); 
+    })
+    $("p").mouseout(function(){
+        $(this).css("background-color", "white")
     })
 })
