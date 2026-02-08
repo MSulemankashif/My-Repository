02bae631dc945c41cab433b1c49ca3532302e8d3
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2026-02-08 16:19:13 +0500

    update

--- Practice2/script.js
+++ Practice2/script.js
@@ -1,15 +1,46 @@
-$(document).ready(function(){
-    $("#hide").click(function(){
-        $("p").hide(700);
-    });
-    $("#show").click(function(){
-        $("p").show(400);
-    })
+// $(document).ready(function(){
+//     $("#hide").click(function(){
+//         $("p").hide(700);
+//     });
+//     $("#show").click(function(){
+//         $("p").show(400);
+//     })
 
-    $("p").hover(function(){
-        $(this).css("background-color", "Lightgray"); 
-    })
-    $("p").mouseout(function(){
-        $(this).css("background-color", "white")
-    })
-})
+//     $("p").hover(function(){
+//         $(this).css("background-color", "Lightgray"); 
+//     })
+//     $("p").mouseout(function(){
+//         $(this).css("background-color", "white")
+//     })
+// })
+
+// let obj = {
+//     name: "Suleman",
+//     age: 20,
+//     city: "Karachi"
+// }
+
+// for(let val in obj){0
+//     console.log(`${val} ${obj[val]}`);
+// }
+
+// let students = ["Suleman", "Ali", "Ahmed", "Umair", "Shahid"];
+
+// for(let i = 0; i<students.length; i++){
+//     if(students[i] === "Ahmed") break;
+//     console.log(students[i]);
+// }
+
+const multiply = (a, b) => a * b;
+
+console.log(multiply(10,20));
+
+
+// "Ternary Opertor"
+// let num = 10;
+// num >= 10 ? console.log("The value is greater than 10 or be 10") : console.log("The valur is less than 10");
+
+
+ function greet(){
+    print("Hello World");
+ }
