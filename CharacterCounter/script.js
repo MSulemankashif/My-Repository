e5b97a203568e2d4e7ebea6892d3061056155806
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2026-03-07 20:03:19 +0500

    update

---
+++ CharacterCounter/script.js
@@ -0,0 +1,10 @@
+let namee = document.getElementById("namee");
+let email = document.getElementById("email");
+let btn = document.querySelector("btn");
+
+
+namee.addEventListener("submit", ()=>{
+    btn.preventDefault()
+    let length = namee.value.length
+    
+})
