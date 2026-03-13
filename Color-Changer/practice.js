6c747c7820ab62b219d1d643d4a4d9a0ebd1b7a1
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2026-03-13 23:15:24 +0500

    Update practice.js

--- Color-Changer/practice.js
+++ Color-Changer/practice.js
@@ -7,3 +7,16 @@ let the = "I love Javascript";
 let pattern = /javascript/i;
 
 console.log(pattern.test(the));
+
+
+let num = "12345678";
+console.log(typeof(num));
+console.log(/^\d+$/.test(num));
+console.log();
+
+
+let username = "Suleman123";
+let patt = /^[a-zA-Z0-9]{3,}$/;
+
+console.log(patt.test(username));
+
